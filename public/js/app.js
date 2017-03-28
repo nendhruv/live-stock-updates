585b3b618b125897a77386ff643348b36fef07c3
nendhruv <nendhruv@gmail.com>
2017-03-29 00:43:49 +0530

    styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
   var stockTable = document.getElementById('stockTable')
-  var stockData = [];
+  stockTable.style.opacity = 0;
 
   var socket = new WebSocket('ws://stocks.mnet.website');
 
@@ -17,35 +17,36 @@ window.onload = function() {
 
 
   var data = {};
-  var company = {}
+  var company = {};
   socket.onmessage = function(event) {
+    stockTable.style.opacity = 0;
     var message = JSON.parse(event.data);
     for (var i in message) {
       company[message[i][0]] = true;
     }
     for(i in message){
-      var t = message[i][0]
-      var h = {}
-      h['prev'] = (data[t]==undefined?message[i][1]:data[t].curr) 
-      h['curr'] = message[i][1]
-      data[t]= h;
+      var token = message[i][0]
+      var value = {}
+      value['prev'] = (data[token]==undefined?message[i][1]:data[token].curr) 
+      value['curr'] = message[i][1]
+      data[token]= value;
     }
     updateData(data, company)
   };
 
 
   function updateData(data, company){
-    var str = ''
+    var str = '<div class="stock head">'+ '<span class="stock-name">Token</span><span class="stock-name">Current Value</span>' +  '<span class="stock-name">Last Updated Value</span></div>'
     for (var i in data) {
       if(company[i]){
         if(data[i]['curr'] - data[i]['prev'] > 0)
-          str += '<li class="received positive">'+ i +  data[i]['curr'] + '     ' + data[i]['prev']+ '</li>';
+          str += '<div class="stock positive">'+ '<span class="stock-name">' + i + '</span><span class="stock-name">' + data[i]['curr'] + ' </span>' +  '<span class="stock-name">'  +  data[i]['prev']+ '</span></div>';
         if(data[i]['curr'] - data[i]['prev'] < 0)
-          str += '<li class="received negative">'+ i +  data[i]['curr'] + '     ' + data[i]['prev']+ '</li>';
+          str += '<div class="stock negative">'+  '<span class="stock-name">'  +  i  +  '</span><span class="stock-name">'  +  data[i]['curr'] + '</span>' + '<span class="stock-name">'+data[i]['prev']+ '</span></div>';
 
       }
       else{
-        str += '<li class="received">'+ i +  data[i]['curr'] + '     ' + data[i]['prev']+ '</li>';
+          str += '<div class="stock">'+ '<span class="stock-name">' + i + '</span><span class="stock-name">' + data[i]['curr'] + ' </span>' +  '<span class="stock-name">'  +  data[i]['prev']+ '</span></div>';
       }
 
     }
@@ -53,6 +54,8 @@ window.onload = function() {
       company[i] = false;
     }
     stockTable.innerHTML = str;
+    stockTable.style.opacity = 1;
+    
   }
 
 };
